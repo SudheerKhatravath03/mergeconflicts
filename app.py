7480c79807cb232675d5ec01dea959a515d72fd8
root <root@ip-172-31-90-111.ec2.internal>
2024-07-01 17:30:03 +0000

    Main updates

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import sys
 
 # assume first name is the first command line argument (after the script name)
-firstname = sys.argv[1]
+firstname = sys.argv[2]
      
-# assume surname is the second command line argument
+# assume surname i1s the second command line argument
 surname = sys.argv[2]
 
 print(f"Your name is {firstname} {surname}")
